5bc4ba7028d2f4a91d4ca050fe724a138de98bb7
nifle3 <kuper116rus.2005@gmail.com>
2024-07-16 22:51:59 +0300

    add: test method for worker

--- worker/app/main.py
+++ worker/app/main.py
@@ -1,4 +1,54 @@
-print("worker work")
+import cv2
+import numpy as np
 
-while True:
+
+def _main():
+    import sys
+    args = sys.argv
+    if len(args) == 1:
+        print('You need input file input')
+        sys.exit(1)
+
+    file_path = args[1]
+    print(f'your file path is {file_path}')
+    img = cv2.imread(filename=file_path)
+    if img is None:
+        print('file cannot read. Please enter someting either')
+        sys.exit(1)
+
+    print('image open')
+
+    try:
+        new_image = pixelize(img)
+        _show_original_and_pixelize(img, new_image)
+    except Exception:
+        pass
+
+
+def pixelize(image: np.ndarray) -> np.ndarray:
+    return image
+
+
+def _unscale(image: np.ndarray) -> np.ndarray:
     pass
+
+
+def _qantization(image: np.ndarray) -> np.ndarray:
+    pass
+
+
+def _upscale(image: np.ndarray) -> np.ndarray:
+    pass
+
+
+def _post_processing(image: np.ndarray) -> np.ndarray:
+    pass
+
+
+def _show_original_and_pixelize(pixelize_image: np.ndarray,
+                                original_image: np.ndarray) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    _main()
